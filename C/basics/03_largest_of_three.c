758347bd90fafa68592e18fb57f0070f817b2f03
pukarplayz <211681449+pukarplayz@users.noreply.github.com>
2026-03-14 20:21:41 +0545

    modified:   C/basics/03_largest_of_three.c 	modified:   README.md

--- C/basics/03_largest_of_three.c
+++ C/basics/03_largest_of_three.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
-int main(void){
-    
+
+int main(void) {
+    int number[4]; 
+    int i;
+
+    for (i = 0; i < 4; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &number[i]);
+    }
+
+    int large = number[0];
+    for (i = 1; i < 4; i++) {
+        if (number[i] > large) {
+            large = number[i];
+        }
+    }
+
+    printf("The largest number is %d\n", large);
 }
