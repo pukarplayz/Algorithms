7b6fd44616ba2b6567975e681e32a4741c063c23
pukarplayz <211681449+pukarplayz@users.noreply.github.com>
2026-03-14 20:42:00 +0545

    new file:   Python/basics/01_swap_numbers.py 	new file:   Python/basics/02_even_or_odd.py

---
+++ Python/basics/01_swap_numbers.py
@@ -0,0 +1,7 @@
+x = 5
+y = 6
+
+x = x+y
+y = x-y
+x=x-y
+print(f"{x=} and {y=}")
---
+++ Python/basics/02_even_or_odd.py
@@ -0,0 +1,7 @@
+number = int(input("Enter your Number: "))
+if number%2==0:
+    print("The number is Even")
+elif number==0:
+    print("The number is Neutral")
+else:
+    print("The number is odd")
